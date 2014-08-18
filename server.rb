529e1bcb4c39ae6058aebbb393aebc9fb64d4368
Szymon Nowak <szimek@gmail.com>
2014-08-18 16:09:34 +0200

    Allow to embed this app in an iframe

--- server.rb
+++ server.rb
@@ -1,6 +1,9 @@
 require "sinatra"
 require "twilio-ruby"
 
+# Allows this app to be embedded in an iframe. Feel free to remove it.
+set :protection, :except => :frame_options
+
 get "/" do
   capability = Twilio::Util::Capability.new ENV["ACCOUNT_SID"], ENV["ACCOUNT_TOKEN"]
   capability.allow_client_outgoing ENV["APP_SID"]
